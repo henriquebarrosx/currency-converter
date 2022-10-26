3afb510c666236e24d79feceb53d01b73bb6176f
Henrique Barros <Henrique.Barros.Xavier@ibm.com>
2022-10-25 23:07:18 -0300

    Remove pino logger dependency

--- src/providers/logger/index.ts
+++ src/providers/logger/index.ts
@@ -1,24 +1,10 @@
-import Pino, { DestinationStream, Logger, LoggerOptions } from "pino"
-
 class Log {
-    public log: Logger<LoggerOptions | DestinationStream>
-
-    constructor() {
-        this.log = Pino({
-            level: "info",
-            prettyPrint: {
-                colorize: true,
-                levelFirst: true,
-            },
-        })
-    }
-
     info(payload: any): void {
-        this.log.info(payload)
+        console.info(payload)
     }
 
     error(payload: any): void {
-        this.log.error(payload)
+        console.error(payload)
     }
 }
 
